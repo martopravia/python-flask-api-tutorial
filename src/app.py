edd066902b8d6beeb0d6da504f28fab48d081e22
Martín Pravia <marto.pravia@gmail.com>
2024-12-04 07:45:32 +0000

    first commit

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if 0 <= position < len(todos):
+        todos.pop(position)
+        return jsonify(todos), 200
+    else:
+        return jsonify({"error": "No existe esa posicion"}), 404
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
